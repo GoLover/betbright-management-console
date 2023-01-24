9b5d2887003e214d6640a122815801c750f17dee
m.tasnimi <m.tasnimi@ompfinex.com>
2023-01-24 17:31:25 +0330

    [ADD] Delete interface

--- domain/sport.go
+++ domain/sport.go
@@ -19,6 +19,7 @@ type SportUseCase interface {
 	UpdateSport(ctx context.Context, sport Sport, sportSlug string) (Sport, error)
 	DeactivateSport(ctx context.Context, slug string) error
 	ActivateSport(ctx context.Context, slug string) error
+	DeleteSport(ctx context.Context, slug string) error
 }
 
 type SportRepository interface {
@@ -26,19 +27,26 @@ type SportRepository interface {
 	GetSportById(id int) (Sport, error)
 	UpdateSport(sport Sport, sportSlug string) (Sport, error)
 	ChangeActivationSport(sportSlug string, active bool) error
+	DeleteSport(slug string) error
+	//---
 	CreateEvent(event Event, sportSlug string) (Event, error)
 	GetEventById(id int) (Event, error)
 	UpdateEvent(event Event, eventSlug, sportSlug string) (Event, error)
 	ChangeActivationEvent(eventSlug string, active bool) (Event, error)
 	GetEventsBySportId(sportId int) ([]Event, error)
+	DeleteEvent(eventSlug string) error
+	//--
 	CreateMarket(market Market, eventSlug string) (Market, error)
 	GetMarketById(id int) (Market, error)
 	UpdateMarket(market Market, marketId int, eventSlug string) (Market, error)
 	ChangeActivationMarket(marketId int, active bool) (Market, error)
 	GetMarketsByEventId(eventId int) ([]Market, error)
+	DeleteMarketById(id int) error
+	//--
 	CreateSelection(selection Selection, marketId, eventId int) (Selection, error)
 	GetSelectionById(id int) (Selection, error)
 	UpdateSelection(selection Selection, selectionId, marketId, eventId int) (Selection, error)
 	ChangeActivationSelection(selectionId int, active bool) (Selection, error)
 	GetSelectionByMarketId(marketId int) ([]Selection, error)
+	DeleteSelectionById(id int) error
 }
